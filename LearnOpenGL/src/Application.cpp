0496d65802de8e395b14b268681936191289f2ec
yuymf <1352948945@qq.com>
2022-08-23 03:34:35 +0800

    since 23

--- LearnOpenGL/src/Application.cpp
+++ LearnOpenGL/src/Application.cpp
@@ -133,11 +133,11 @@ int main(void)
         ImGui_ImplGlfw_InitForOpenGL(window, true);
         ImGui_ImplOpenGL3_Init(glsl_version);
 
-        //glm::vec3 translationA(200, 200, 0);
-        //glm::vec3 translationB(400, 200, 0);
-        ////danamic color based on uniform u_Color£»
-        //float r = 0.0f;
-        //float increment = 0.05f;
+        glm::vec3 translationA(200, 200, 0);
+        glm::vec3 translationB(400, 200, 0);
+        //danamic color based on uniform u_Color£»
+        float r = 0.0f;
+        float increment = 0.05f;
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
@@ -149,49 +149,49 @@ int main(void)
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
-            //{
-            //    glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-            //    glm::mat4 mvp = proj * view * model;
-            //    shader.Bind();
-            //    //glUseProgram(shader);
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                //glUseProgram(shader);
 
-            //    //glDrawArrays(GL_TRIANGLES, 0, 3);
-            //    //glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
-            //    //shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-            //    shader.SetUniformMat4f("u_MVP", mvp);
-            //    //glBindVertexArray(vao);
-            //    renderer.Draw(va, ib, shader);
-            //}
+                //glDrawArrays(GL_TRIANGLES, 0, 3);
+                //glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
+                //shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+                shader.SetUniformMat4f("u_MVP", mvp);
+                //glBindVertexArray(vao);
+                renderer.Draw(va, ib, shader);
+            }
 
-            //{
-            //    glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-            //    glm::mat4 mvp = proj * view * model;
-            //    shader.Bind();
-            //    shader.SetUniformMat4f("u_MVP", mvp);
-            //    renderer.Draw(va, ib, shader);
-            //}
-            ////glBindBuffer(GL_ARRAY_BUFFER, buffer);
-            ////va.Bind(); //in this addbuffer, vb already bind;
-            ////vb.Bind();
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
+                renderer.Draw(va, ib, shader);
+            }
+            //glBindBuffer(GL_ARRAY_BUFFER, buffer);
+            //va.Bind(); //in this addbuffer, vb already bind;
+            //vb.Bind();
 
-            ////glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-            ////ib.Bind();
+            //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+            //ib.Bind();
 
             ////GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
-            //if (r > 1.0f)
-            //    increment = -0.05f;
-            //else if (r < 0.0f)
-            //    increment = 0.05f;
-            //r += increment;
+            if (r > 1.0f)
+                increment = -0.05f;
+            else if (r < 0.0f)
+                increment = 0.05f;
+            r += increment;
 
-            //// 2. Show a simple window that we create ourselves. 
-            ////We use a Begin/End pair to created a named window.
-            //{         
-            //    ImGui::SliderFloat3("Tanslation A", &translationA.x, 0.0f, 960.0f);
-            //    ImGui::SliderFloat3("Tanslation B", &translationB.x, 0.0f, 960.0f);
-            //    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            //}
+            // 2. Show a simple window that we create ourselves. 
+            //We use a Begin/End pair to created a named window.
+            {         
+                ImGui::SliderFloat3("Tanslation A", &translationA.x, 0.0f, 960.0f);
+                ImGui::SliderFloat3("Tanslation B", &translationB.x, 0.0f, 960.0f);
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            }
 
             ImGui::Render();
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
